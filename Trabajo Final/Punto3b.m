e62051f2f79168514da9226dc7a8b03ca234b9bf
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-11-17 18:28:07 -0500

    Update Punto3b.m

--- Trabajo Final/Punto3b.m
+++ Trabajo Final/Punto3b.m
@@ -4,11 +4,11 @@ a = 0;
 b = 1;
 at = 0;
 bt = 1;
-f = @(x,t)4*exp(-x-2*t);
-g1 = @(x)exp(-x);
-g2 = @(x)-2*exp(-x);
-g3 = @(t)exp(-2*t);
-g4 = @(t)exp(-1-2*t);
+dd = @(x,t)4*exp(-x-2*t); %Segunda derivdada de u
+f = @(x)exp(-x); %Condicion inicial
+g= @(x)-2*exp(-x); %Condicion inicial
+l = @(t)exp(-2*t); %Condicion de frontera
+r = @(t)exp(-1-2*t); %Condicion de frontera
 m =M+1; n=N+1; mn = m*n;
 h = (b-a)/M; 
 k =(bt-at)/4; 
@@ -20,18 +20,18 @@ b= zeros(mn,1);
 for i=1:m
     j =1;
     A(i+(j-1)*m, i+(j-1)*m) = 1;
-    b(i+(j-1)*m) = g1(x(i));
+    b(i+(j-1)*m) = f(x(i));
     j=n;
     A(i+(j-1)*m, i+(j-1)*m)=1;
-    b(i+(j-1)*m) = g2(x(i));
+    b(i+(j-1)*m) = g(x(i));
 end
 for j=2:n-1
     i =1;
     A(i+(j-1)*m, i+(j-1)*m) = 1;
-    b(i+(j-1)*m) = g3(t(j));
+    b(i+(j-1)*m) = l(t(j));
     i = m;
     A(i+(j-1)*m, i+(j-1)*m) = 1;
-    b(i+(j-1)*m) = g4(t(j));
+    b(i+(j-1)*m) = r(t(j));
 end
 
 for i=2:m-1
@@ -41,7 +41,7 @@ for i=2:m-1
      A(i+(j-1)*m, i+(j-1)*m) = -3/h^2-2/k^2;
      A(i+(j-1)*m, i+(j-2)*m) = 1/(k^2);
      A(i+(j-1)*m, i+j*m) = 1/(k^2);
-     b(i+(j-1)*m)=f(x(i),t(j));
+     b(i+(j-1)*m)=dd(x(i),t(j));
     end
 end
 v=A\b
