327f0d6cf4ae5231bd6970d21a3cb3f96d2397bb
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-11-16 23:46:06 -0500

    Add files via upload

---
+++ Trabajo Final/Punto3b.m
@@ -0,0 +1,45 @@
+M = 20;
+N = 20;
+a = 0;
+b = 1;
+at = 0;
+bt = 1;
+f = @(x,t)4*exp(-x-2*t);
+g1 = @(x)exp(-x);
+g2 = @(x)-2*exp(-x);
+g3 = @(t)exp(-2*t);
+g4 = @(t)exp(-1-2*t);
+m =M+1; n=N+1; mn = m*n;
+h = (b-a)/M; h2 = h^2; k =(bt-at)/4; k2 = k^2;
+x = a+(0:M)*h;
+t = at+(0:N)*k;
+A = zeros(mn,mn); b= zeros(mn,1);
+for i=2:m-1
+    for j=2:n-1
+     A(i+(j-1)*m, i-1+(j-1)*m) = 1/(h2);
+     A(i+(j-1)*m, i+1+(j-1)*m) = 1/(h2);
+     A(i+(j-1)*m, i+(j-1)*m) = -3/h2-2/k2;
+     A(i+(j-1)*m, i+(j-2)*m) = 1/(k2);
+     A(i+(j-1)*m, i+j*m) = 1/(k2);
+     b(i+(j-1)*m)=f(x(i),t(j));
+    end
+end
+for i=1:m
+    j =1;
+    A(i+(j-1)*m, i+(j-1)*m) = 1;
+    b(i+(j-1)*m) = g1(x(i));
+    j=n;
+    A(i+(j-1)*m, i+(j-1)*m)=1;
+    b(i+(j-1)*m) = g2(x(i));
+end
+for j=2:n-1
+    i =1;
+    A(i+(j-1)*m, i+(j-1)*m) = 1;
+    b(i+(j-1)*m) = g3(t(j));
+    i = m;
+    A(i+(j-1)*m, i+(j-1)*m) = 1;
+    b(i+(j-1)*m) = g4(t(j));
+end
+v=A\b
+w=reshape(v(1:mn),m,n);
+mesh(x,t,w)
