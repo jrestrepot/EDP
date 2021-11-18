498835951dd2e46d75e9e0ed187e471fc884e422
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-11-18 15:35:23 -0500

    Se añaden comentarios

--- Trabajo Final/Punto3b.m
+++ Trabajo Final/Punto3b.m
@@ -1,11 +1,12 @@
+%Código basado en el esquema matricial
 c = 2;
-m = 20;
-f = @(x)exp(-x);
-g = @(x)-2*exp(-x);
-l = @(t)exp(-2*t);
-r = @(t) exp(-1-2*t);
-h = 1/m;
-k = h/(c*2); 
+m = 20; %#intervalos de x
+f = @(x)exp(-x); &%cond inicial
+g = @(x)-2*exp(-x); %cond inicial
+l = @(t)exp(-2*t); %cond frontera
+r = @(t) exp(-1-2*t); %cond frontera
+h = 1/m; %definición de h
+k = h/(c*2);  %garantiza estabilidad
 sigma = c*k/h;
 n = m;
 a = 0;
@@ -24,23 +25,25 @@ T0(1) = l(0);
 T0(M) = r(0);
 
 for i=1:M
-   Fx(i,1) = f(X(i)); 
-   Gx(i,1) = g(X(i));
+   Fx(i,1) = f(X(i)); %sols de fx
+   Gx(i,1) = g(X(i));  %sols de gx
 end
 
 W0 = Fx;
-W1 = (1/2)*A*Fx +k*Gx+(sigma^2)/2*T0;
+W1 = (1/2)*A*Fx +k*Gx+(sigma^2)/2*T0; %Primer paso temporal
 Wij = ones(M,N);
-Wij(:,1) = W0;
-Wij(:,2) = W1;
+Wij(:,1) = W0; %Se define la primera columna de la matriz como W0
+Wij(:,2) = W1; %Se define la segunda columna de la matriz como W1
+
 for i=3:N
     for j=2:M
         T0(1)= l(T(j-1));
         T0(M)= r(T(j-1));
-    end
-    W = A*Wij(:,i-1)-Wij(:,i-2)+sigma^2*T0;
+        Wij(:,i) = A*Wij(:,i-1)-Wij(:,i-2)+sigma^2*T0; %matriz de soluciones, se llena por columnas
+    end   
 end
 
+%Si se quieren ver las soluciones imprima Wij
 
 %%Solución real
 ur = @(x,t) exp(-x-2*t);
